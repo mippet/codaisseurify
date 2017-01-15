f4c31825504ba4a9a4af4816eb272214bcce332d
mippet <pimluitjes@gmail.com>
2017-01-15 13:16:12 +0100

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Song.delete_all
+Artist.delete_all
+
+
+abba = Artist.create ({name:"ABBA"})
+beatles = Artist.create ({name:"Beatles"})
+
+song1 = Song.create ({title:"Mama Mia", artist: abba})
+song2 = Song.create ({title:"Voulez Vous", artist: abba})
+song3 = Song.create ({title:"S.O.S", artist: abba})
+
+song4 = Song.create ({title:"Help", artist: beatles})
+song5 = Song.create ({title:"Let it Be", artist:beatles})
+song6 = Song.create ({title:"Twist and Shout", artist:beatles})
