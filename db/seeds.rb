bccb264f763dc4994abeca3fdf80e0e30932cc5b
mippet <pimluitjes@gmail.com>
2017-01-15 13:42:28 +0100

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,31 @@
 Song.delete_all
 Artist.delete_all
 
+song1 = Song.create ({title:"Mama Mia"})
+song2 = Song.create ({title:"Voulez Vous"})
+song3 = Song.create ({title:"S.O.S"})
 
-abba = Artist.create ({name:"ABBA"})
-beatles = Artist.create ({name:"Beatles"})
+song4 = Song.create ({title:"Help"})
+song5 = Song.create ({title:"Let it Be"})
+song6 = Song.create ({title:"Twist and Shout"})
 
-song1 = Song.create ({title:"Mama Mia", artist: abba})
-song2 = Song.create ({title:"Voulez Vous", artist: abba})
-song3 = Song.create ({title:"S.O.S", artist: abba})
+artist1 = Artist.create ({name:"ABBA"})
+artist2 = Artist.create ({name:"Beatles"})
 
-song4 = Song.create ({title:"Help", artist: beatles})
-song5 = Song.create ({title:"Let it Be", artist:beatles})
-song6 = Song.create ({title:"Twist and Shout", artist:beatles})
+artist1.songs << [song1]
+artist1.save
+
+artist1.songs << [song2]
+artist1.save
+
+artist1.songs << [song3]
+artist1.save
+
+artist2.songs << [song4]
+artist2.save
+
+artist2.songs << [song5]
+artist2.save
+
+artist2.songs << [song6]
+artist2.save
