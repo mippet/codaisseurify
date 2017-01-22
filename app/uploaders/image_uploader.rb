d4479feb1d11ed84702ddb4dc39cef4f4a44f7e2
mippet <pimluitjes@gmail.com>
2017-01-22 19:28:51 +0100

    some random stuff

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,15 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  version :small do
-   eager
-   resize_to_fit(438, 250)
-   cloudinary_transformation :quality => 80
- end
-
- version :large do
-   eager
-   resize_to_fit(450, 450)
-   cloudinary_transformation :quality => 80
- end
+  version :thumbnail do
+      eager
+      resize_to_fit(250, 250)
+      cloudinary_transformation :quality => 80
+    end
 end
