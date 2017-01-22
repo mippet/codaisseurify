855297cba15deeb8593e335d311eb1115098e95c
mippet <pimluitjes@gmail.com>
2017-01-22 12:27:00 +0100

    added artists index

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 Song.delete_all
 Artist.delete_all
 
-song1 = Song.create ({title:"Mama Mia"})
-song2 = Song.create ({title:"Voulez Vous"})
-song3 = Song.create ({title:"S.O.S"})
+song1 = Song.create({title:"Mama Mia"})
+song2 = Song.create({title:"Voulez Vous"})
+song3 = Song.create({title:"S.O.S"})
 
-song4 = Song.create ({title:"Help"})
-song5 = Song.create ({title:"Let it Be"})
-song6 = Song.create ({title:"Twist and Shout"})
+song4 = Song.create({title:"Help"})
+song5 = Song.create({title:"Let it Be"})
+song6 = Song.create({title:"Twist and Shout"})
 
-artist1 = Artist.create ({name:"ABBA"})
-artist2 = Artist.create ({name:"Beatles"})
+artist1 = Artist.create({name:"ABBA"})
+artist2 = Artist.create({name:"Beatles"})
 
 artist1.songs << [song1]
 artist1.save
