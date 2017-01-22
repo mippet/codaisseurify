71c25fe6744b7d9e57623cfb6a1d8c1ab8e7f10b
mippet <pimluitjes@gmail.com>
2017-01-22 12:31:09 +0100

    added sorting by asc-desc artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,12 @@
 class ArtistsController < ApplicationController
 
   def index
-    @artists = Artist.all
+    @artists = Artist.all.order(params[:sort])
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name)
   end
 end
